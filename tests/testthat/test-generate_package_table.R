9d7d20d2ab1475cc18e4a080259ed3d2c4835120
Jackson Hoffart <jackson.hoffart@gmail.com>
2024-04-08 15:55:00 +0200

    add some simple tests

--- tests/testthat/test-generate_package_table.R
+++ tests/testthat/test-generate_package_table.R
@@ -1,3 +1,20 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(testthat)
+library(dplyr)
+
+pacta_r_package_path <- "https://github.com/RMI-PACTA/pacta.r.package"
+
+test_that("returns correct structure and data", {
+  result <- generate_package_table(pacta_r_package_path)
+  expected_names <- c("Repo", "Lifecycle", "Status", "Latest_SHA", "Maintainer")
+
+  expect_true(tibble::is_tibble(result))
+  expect_equal(ncol(result), 5)
+  expect_true(all(expected_names %in% names(result)))
+})
+
+test_that("handles empty input", {
+  empty_input <- character(0)
+  result <- generate_package_table(empty_input)
+
+  expect_true(tibble::is_tibble(result) && nrow(result) == 0)
 })
