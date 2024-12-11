ee14e0568e384a14469776e7987cb4522b16320b
Jackson Hoffart <jackson.hoffart@gmail.com>
2024-12-12 00:08:29 +0100

    add format_badges util functions

---
+++ R/format_badges.R
@@ -0,0 +1,55 @@
+format_badge_url <- function(display, path) {
+  glue::glue("[{display}]({path})")
+}
+
+format_name_badge <- function(repo_path) {
+  repo_org <- strsplit(repo_path, "/")[[1]][1]
+  repo_name <- strsplit(repo_path, "/")[[1]][2]
+
+  format_badge_url(
+    display = repo_name,
+    path = glue::glue("https://{tolower(repo_org)}.github.io/{repo_name}/")
+  )
+}
+
+format_lifecycle_badge <- function(lifecycle) {
+  format_badge_url(
+    display = glue::glue(
+      "![](https://img.shields.io/badge/lifecycle-{lifecycle}-none.svg?labelColor=%23444d56&amp;color=%2334d058)"
+    ),
+    path = glue::glue("https://lifecycle.r-lib.org/articles/stages.html#{lifecycle}")
+  )
+}
+
+format_status_badge <- function(repo_path, ci_check) {
+  format_badge_url(
+    display = glue::glue(
+      "![](https://github.com/{repo_path}/actions/workflows/{ci_check}/badge.svg?branch=main)"
+    ),
+    path = glue::glue("https://github.com/{repo_path}/actions/workflows/{ci_check}?query=branch%3Amain")
+  )
+}
+
+format_coverage_badge <- function(repo_path) {
+  format_badge_url(
+    display = glue::glue("![](https://img.shields.io/codecov/c/github/{tolower(repo_path)}/main)"),
+    path = glue::glue("https://app.codecov.io/gh/{repo_path}?branch=main")
+  )
+}
+
+format_version_badge <- function(repo_path) {
+  format_badge_url(
+    display = glue::glue("![](https://img.shields.io/github/r-package/v/{tolower(repo_path)}/main?label=version&amp;labelColor=%23444d56&amp;color=%2334d058)"),
+    path = glue::glue("https://github.com/{repo_path}/blob/main/DESCRIPTION")
+  )
+}
+
+format_maintainer_badge <- function(repo_path) {
+  repo_org <- strsplit(repo_path, "/")[[1]][1]
+  repo_name <- strsplit(repo_path, "/")[[1]][2]
+
+  format_badge_url(
+    display = glue::glue("![](https://img.shields.io/badge/dynamic/json?label=codeowner&amp;query=codeownerInfo.ownersForFile&amp;url=https%3A%2F%2Fgithub.com%2F{tolower(repo_org)}%2F{tolower(repo_name)}%2Fdeferred-metadata%2Fmain%2F.github%2FCODEOWNERS)"),
+    path = glue::glue("https://github.com/{repo_path}/blob/main/.github/CODEOWNERS")
+  )
+}
